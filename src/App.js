9ff2a8b1183fbe677d3d6bf0eec6e24902c9ff7c
Dawid Humienik <dawid.humienik@gmail.com>
2022-06-23 17:46:53 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,12 +38,23 @@ function App() {
     })));
   };
 
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
 	return (
 		<Container>
 			<Header title="Lista zadań" />
 			<Section
 				title="Dodaj nowe zadanie"
-				body={<Form />}
+				body={<Form addNewTask={addNewTask} />}
 			/>
 
 			<Section
