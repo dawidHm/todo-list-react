b2cb655068aa5166866509f1b64c36025b6602f5
Dawid Humienik <dawid.humienik@gmail.com>
2022-06-23 15:02:52 +0200

    toggleHideDone interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./style.css";
 
 
-const Buttons = ({ tasks, hideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
     <div className="section__filterButtonsContainer">
         {tasks.length > 0 && (
             <React.Fragment>
-                <button className="section__filterButton">
-                    {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
+                <button onClick={toggleHideDone} className="section__filterButton">
+                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button
                     className="section__filterButton"
